bcf914959ed2bc3614c7ea8b16b81685df5ab59c
Daniel Olson <emaildano@gmail.com>
2018-11-26 15:38:09 -0500

    Shifter status config

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "System Status",
+  "name": "shifter_status",
+  "description": "Welcome to Shifter Status, a place for activity logs and system performance.",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "json"
+  }
+}
